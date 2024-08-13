fbf9fabbdf6db84df1d907130f286504cf6a9650
Samuel Perez Tovar <perezsamuelxzw@hotmail.com>
2024-08-13 10:39:44 -0500

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button colorScheme="blue">Submit</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="lightblue">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gray">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="yellow">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;

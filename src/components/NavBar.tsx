9c82bd3a2082490f2db6b4824271a97f0a4c0b75
Samuel Perez Tovar <perezsamuelxzw@hotmail.com>
2024-09-16 22:17:16 -0400

    Replace logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/image.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="70px" width="auto" />
       <ColorModeSwitch />
     </HStack>
   );
